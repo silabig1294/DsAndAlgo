c56c757b82877a1b8acd99adcc03d1d6edc7fdc3
Sila Siangchin <siangchin_s@silpakorn.edu>
2022-09-19 11:12:47 +0700

    add bubblesort

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
-	"sort"
-
+	// "sort"
+	s "github.com/silabig1294/DsAndAlgo/bubble_sort"
 	b "github.com/silabig1294/DsAndAlgo/binary_search"
 	l "github.com/silabig1294/DsAndAlgo/linear_search"
 )
 
 func main() {
 	items := []int{95, 78, 46, 58, 45, 86, 99, 251, 320}
-	sort.Ints(items)
+	s.Bubblesort(items)
+	// sort.Ints(items)
 	val := l.Linearsearch(items,45)
 	val2 := b.BinarySearch(items,320)
 	fmt.Println(items)
